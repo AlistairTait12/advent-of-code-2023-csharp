00388d9b0ecab63bc6cc4f3405eff5a37062b1e6
Alistair Tait <alistair.tait1@gmail.com>
2023-12-02 10:58:42 +0000

    Initial commit, working on day 1 challenge, created a working line processor to get calibration value from a line of text in the calibration document

---
+++ AdventOfCode/DayOne/CalibrationLineProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.DayOne;
+
+public static class CalibrationLineProcessor
+{
+    public static int GetValueFromLine(string line)
+    {
+        var digitRegex = new Regex(@"\d");
+        var matches = digitRegex.Matches(line);
+
+        if (matches.Count is 0)
+        {
+            return 0;
+        }
+
+        int.TryParse($"{matches.First()}{matches.Last()}", out var result);
+        return result;
+    }
+}
---
+++ AdventOfCodeTests/DayOneTests/CalibrationLineProcessorTests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.DayOne;
+
+namespace AdventOfCodeTests.DayOneTests;
+
+[TestFixture]
+public class CalibrationLineProcessorTests
+{
+    [TestCase("9cbncbxclbvkmfzdnldc", 99)]
+    [TestCase("treb7uchet", 77)]
+    public void GetValueFromLineWithOnlyOneDigitReturnsDigitTwiceAsInteger(string line, int expected)
+    {
+        // Act
+        var actual = CalibrationLineProcessor.GetValueFromLine(line);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("1abc2", 12)]
+    [TestCase("pqr3stu8vwx", 38)]
+    [TestCase("a1b2c3d4e5f", 15)]
+    public void GetValueFromLineWithMultipleDigitsReturnsFirstAndLastDigitsAsInt(string line, int expected)
+    {
+        // Act
+        var actual = CalibrationLineProcessor.GetValueFromLine(line);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("NoDigitsHere")]
+    [TestCase("lskdhfkjsdhgkjf")]
+    [TestCase("No digits here")]
+    public void GetValueWhenThereAreNoDigitsReturnsZero(string line)
+    {
+        // Act
+        var actual = CalibrationLineProcessor.GetValueFromLine(line);
+
+        // Assert
+        Assert.That(actual, Is.Zero);
+    }
+}
