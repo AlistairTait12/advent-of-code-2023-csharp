5b4f59c8c77a685250bd234f6b6e8b37b0a9531c
Alistair Tait <alistair.tait1@gmail.com>
2023-12-02 11:41:15 +0000

    Got the answer to get first gold star of day 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Ho, ho, ho!");
+﻿using AdventOfCode.DayOne;
+
+Console.WriteLine("Ho, ho, ho!");
 
 // Area for day 1 answers
+var calibrationDocProcessor = new CalibrationDocumentProcessor(Path.Combine(
+    AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\DayOne\day_1_puzzle_input.txt"));
+Console.WriteLine(calibrationDocProcessor.GetValueFromDocument());
