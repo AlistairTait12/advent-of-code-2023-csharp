3de79d5ccb6a7c211f04d8d343e9e3df9f8bc677
Alistair Tait <alistair.tait1@gmail.com>
2023-12-02 22:05:24 +0000

    Tidied up CalibrationLineProcessor

--- AdventOfCode/DayOne/CalibrationLineProcessor.cs
+++ AdventOfCode/DayOne/CalibrationLineProcessor.cs
@@ -4,6 +4,8 @@ namespace AdventOfCode.DayOne;
 
 public static class CalibrationLineProcessor
 {
+    private static readonly string _digitsPattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
+
     private static readonly Dictionary<string, int> _numbersAsWords = new()
     {
         ["one"] = 1,
@@ -19,54 +21,38 @@ public static class CalibrationLineProcessor
 
     public static int GetValueFromLine(string line)
     {
-        // Get all matches
-        var digitRegex = new Regex(
-            @"\d|one|two|three|four|five|six|seven|eight|nine",
-            RegexOptions.IgnoreCase);
-        var matches = digitRegex.Matches(line);
-        
-        // Get all matches where words potentially overlap
-        var reversedRegex = new Regex(
-            @"\d|one|two|three|four|five|six|seven|eight|nine",
-            RegexOptions.RightToLeft);
-
-        var reversedMatches = reversedRegex.Matches(line);
+        var matches = new Regex(_digitsPattern, RegexOptions.IgnoreCase)
+            .Matches(line);
 
         if (matches.Count is 0)
         {
             return 0;
         }
 
-        // Map MatchCollection to a List<int>
-        var converted = GetDigitsFromLine(matches);
+        var reversedMatches = new Regex(_digitsPattern, RegexOptions.IgnoreCase | RegexOptions.RightToLeft)
+            .Matches(line);
+
+        var digitsAsInts = GetDigitsFromLine(matches);
         var reversedConverted = GetDigitsFromLine(reversedMatches);
 
-        converted.Add(reversedConverted.First());
+        digitsAsInts.Add(reversedConverted.First());
 
-        int.TryParse($"{converted.First()}{converted.Last()}", out var result);
+        int.TryParse($"{digitsAsInts.First()}{digitsAsInts.Last()}", out var result);
         return result;
     }
 
     private static List<int> GetDigitsFromLine(MatchCollection matches)
     {
-        var digits = matches.Select(match =>
+        return matches.Select(match =>
         {
-            var pattern = new Regex(@"\d");
             int convertedDigit;
-
-            if (!pattern.IsMatch(match.Value))
+            if (!int.TryParse(match.Value, out convertedDigit))
             {
                 convertedDigit = GetNumberFromWord(match.Value);
             }
-            else
-            {
-                int.TryParse(match.Value, out convertedDigit);
-            }
 
             return convertedDigit;
-        });
-
-        return digits.ToList();
+        }).ToList();
     }
 
     private static int GetNumberFromWord(string word) => _numbersAsWords[word.ToLower()];
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,4 +6,9 @@ Console.WriteLine("Ho, ho, ho!");
 var fileWrapper = new RealFileWrapper(Path.Combine(
     AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\DayOne\day_1_puzzle_input.txt"));
 var calibrationDocProcessor = new CalibrationDocumentProcessor(fileWrapper);
-Console.WriteLine(calibrationDocProcessor.GetValueFromDocument());
+var dayOnePartTwoAnswer = calibrationDocProcessor.GetValueFromDocument();
+
+Console.WriteLine($"The answer to day 1 part 1 is: 54953");
+Console.WriteLine($"The answer to day 1 part 2 is: {dayOnePartTwoAnswer}");
+
+// Area for day 2 answers
